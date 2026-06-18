948bdfacdfd8ef612a50ad0ed105f2d65b795e69
Pushpak Chhajed <pushpak1300@gmail.com>
2026-06-18 15:57:05 +0530

    Remove experimental label from parallel option

--- app/Commands/DefaultCommand.php
+++ app/Commands/DefaultCommand.php
@@ -51,7 +51,7 @@ class DefaultCommand extends Command
                     new InputOption('output-format', '', InputOption::VALUE_REQUIRED, 'The format that should be used when outputting the test results to a file'),
                     new InputOption('stdin-filename', null, InputOption::VALUE_REQUIRED, 'File path context for stdin input'),
                     new InputOption('cache-file', '', InputArgument::OPTIONAL, 'The path to the cache file'),
-                    new InputOption('parallel', 'p', InputOption::VALUE_NONE, 'Runs the linter in parallel (Experimental)'),
+                    new InputOption('parallel', 'p', InputOption::VALUE_NONE, 'Runs the linter in parallel'),
                     new InputOption('max-processes', null, InputOption::VALUE_REQUIRED, 'The number of processes to spawn when using parallel execution'),
                 ],
             );
